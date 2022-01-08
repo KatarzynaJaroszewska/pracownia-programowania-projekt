aa14b28b3e8a15164de336329f616f6da248bc8b
Katarzyna Jaroszewska <katarzyna_jaroszewska@wp.eu>
2022-01-08 22:34:40 +0100

    PracowniaProgramowaniaProjektApplicationTests - add ActiveProfiles annotation (with "hsql" param)

--- src/test/java/com/uam/pracowniaprogramowaniaprojekt/PracowniaProgramowaniaProjektApplicationTests.java
+++ src/test/java/com/uam/pracowniaprogramowaniaprojekt/PracowniaProgramowaniaProjektApplicationTests.java
@@ -2,8 +2,10 @@ package com.uam.pracowniaprogramowaniaprojekt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("hsql")
 class PracowniaProgramowaniaProjektApplicationTests {
 
 	@Test
